e861b3b8f95cea9a2e65b052654f68eaefcc2d16
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-05-07 12:27:29 +0200

    js 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,32 @@ var jeu = true;
 var text = "";
 var plateau = [];
 
-for (var i = 0; i < nbLigne.length; i++) {
-	plateau[i] += [];
+for (var i = 0; i < nbLigne; i++) {
+	plateau[i] = [];
+}
+
+newGame();
+
+function newGame() {
+	for (var i = 0; i < nbLigne; i++) {
+		for (var j = 0; j < nbColonne; j++) {
+			plateau[i][j] = 0;
+		}
+	}
+	var joueur = 1;
+	afficheTextAnnonce("Le jeu commence ! C'est le tour du joueur "+nomDuJoueur(joueur));
+	var jeu = true;
+	creerTableau();
+}
+
+afficheTextAnnonce(text) {
+	document.getElementById('textAnnonce').innerHTML = text;
+}
+
+nomDuJoueur(nbJoueur) {
+	if (nbJoueur == 1) {
+		return "rouge";
+	} else {
+		return "bleu";
+	}
 }
