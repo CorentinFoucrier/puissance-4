baaa290c15682c289877f7a44a8941381eb88e76
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-05-10 11:20:03 +0200

    comments and mini

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,9 @@ function nomDuJoueur(nbJoueur) {
 	}
 }
 
+/* Créer le tableau en avec le nombre de lignes et colonnes définit
+** dans les var nbLigne et nbColonne
+*/
 function creerTableau() {
 	txt = "<table>";
 	for (var i = 0; i < nbLigne; i++) {
@@ -56,18 +59,24 @@ function creerTableau() {
 }
 
 function detectClick(j) {
+	// s'il reste une case de libre dans la colonne et si e jeu est en cours
 	if (verifPosition(j) && jeu) {
-		var ligneEnCour = poseJeton(j); //numéro de la ligne en cour.
-		var verifEnd = puissance4(ligneEnCour, j, 0, 0);
+		var ligneEnCour = poseJeton(j); // numero de la ligne en cours
+		var verifEnd = puissance4(ligneEnCour, j, 0, 0); // la vérif si vainqueur
 		if (verifEnd) {
 			jeu = false;
 			afficheTextAnnonce('Le joueur '+nomDuJoueur(nbJoueur)+' a gagné la partie !');
 		} else {
-			if (nbJoueur == 1) {
-				nbJoueur = 2;
-			} else {
-				nbJoueur = 1;
-			}
+			nbJoueur == 1 ? nbJoueur = 2 : nbJoueur = 1;
+			/*	TERNAIRE
+			**
+			**	if (nbJoueur == 1){
+			**		nbJoueur = 2;
+			**	}else{
+			**		nbJoueur = 1;
+			**	}
+			**
+			*/
 			afficheTextAnnonce('c\'est au tour du joueur '+nomDuJoueur(nbJoueur));
 		}
 	}
@@ -81,6 +90,7 @@ function verifPosition(j) {
 	}
 }
 
+/* no de la ligne disponible où le jeton peut etre posé */
 function poseJeton(j) {
 	for (var i = nbLigne-1; i >= 0; i--) {
 		if (plateau[i][j] == 0) {
@@ -98,26 +108,37 @@ function refreshTableau(x, y, i) {
 function puissance4(ligne, colonne, l, c){
     console.log('valeur : '+ligne+' '+colonne+' / incremente '+l+' '+c);
     if (c == 0 && l == 0) {
+    	/* Ce 1er appel lance les appels récursifs
+		** -1 pour decaler d'une colonne à gauche ou en haut (et ne pas revenir dans cette condition 0 0) 
+		** 1 pour decaler d'une colonne à droite ou en bas (et ne pas revenir dans cette condition 0 0)
+		*/
+
         //horizontal
         var va = 1 + puissance4(ligne, colonne-1, 0, -1) + puissance4(ligne, colonne+1, 0, 1);
+        
         //vertical
         var vb = 1 + puissance4(ligne-1, colonne, -1, 0) + puissance4(ligne+1, colonne, 1, 0);
-        //diag gauche
+        
+        //diagonale qui descend vers la gauche
         var vc = 1 + puissance4(ligne-1, colonne-1, -1, -1) + puissance4(ligne+1, colonne+1, 1, 1);
-        //diag droite
+        
+        // diagonale qui descend vers la droite
         var vd = 1 + puissance4(ligne-1, colonne+1, -1, +1) + puissance4(ligne+1, colonne-1, 1, -1);
 
+        //console.log(va,vb,vc,vd);
         console.log(va,vb,vc,vd);
-        if (va == 4 || vb == 4 || vc == 4 || vd == 4) {
+        if (va >= 4 || vb >= 4 || vc >= 4 || vd >= 4) {
             return true;
         } else {
             return false;
         }
     }
 
+    // condition terminale
     if (ligne < nbLigne && ligne >= 0 && colonne < nbColonne && colonne >= 0) {
         console.log('valeur : '+ligne+' '+colonne+' / incremente '+l+' '+c);
         if (plateau[ligne][colonne] == nbJoueur) {
+        	// boucle récursive avec le décalage d'entrée (-1 ou +1) pour voir les cases suivantes
             return 1 + puissance4(ligne + l, colonne + c, l, c);
         } else {
             return 0;
